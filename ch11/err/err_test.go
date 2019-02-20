cb5ce5358a58728ef6541f8068e9560e3d13d6b8
chaocai <chaocai@192.168.0.106>
2019-02-20 22:38:12 +0800

    synced

--- ch11/err/err_test.go
+++ ch11/err/err_test.go
@@ -5,10 +5,19 @@ import (
 	"testing"
 )
 
+var LessThanTwoError error = errors.New("n must be greater than 2")
+var GreaterThanHundredError error = errors.New("n must be less than 100")
+
 func GetFibonacci(n int) ([]int, error) {
 	fibList := []int{1, 1}
-	if n < 0 || n > 100 {
-		return nil, errors.New("n must be in the range [0,100]")
+	// if n < 2 || n > 100 {
+	// 	return nil, errors.New("n must be in the range [0,100]")
+	// }
+	if n < 0 {
+		return nil, LessThanTwoError
+	}
+	if n > 100 {
+		return nil, GreaterThanHundredError
 	}
 
 	for i := 2; /*短变量声明 := */ i < n; i++ {
@@ -18,5 +27,19 @@ func GetFibonacci(n int) ([]int, error) {
 }
 
 func TestGetFibonacci(t *testing.T) {
+	var list []int
+	list, err := GetFibonacci(-10)
+	if err == LessThanTwoError {
+		t.Error("Need a larger number")
+	}
+
+	if err == GreaterThanHundredError {
+		t.Error("Need a larger number")
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(list)
 
 }
